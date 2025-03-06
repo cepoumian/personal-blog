fadb6d881a79e58adfb7b2e7369960d262aac7a5
cepoumian <cepoumian@gmail.com>
2025-03-05 20:02:53 -0600

    Create navbar

---
+++ src/data/siteData.ts
@@ -0,0 +1,18 @@
+export const baseData = {
+  title: "Cesar Poumian's Blog",
+  description:
+    "Welcome to my blog! I'm a software engineer and I write about programming, tech, and personal development.",
+};
+
+export const socialData = [
+  {
+    title: "linkedin",
+    href: "https://www.linkedin.com/in/cesar-eduardo-poumian-orozco-407020b6/",
+    icon: "icon--linkedin",
+  },
+  {
+    title: "github",
+    href: "https://github.com/",
+    icon: "icon--github",
+  },
+];
