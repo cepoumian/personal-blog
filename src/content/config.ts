14fa97648ce0f6b7fa945b91a3c005f231b31931
cepoumian <cepoumian@gmail.com>
2025-03-07 10:18:35 -0600

    Create home with intro and latest articles sections

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+import { defineCollection, reference, z } from "astro:content";
+
+const posts = defineCollection({
+  type: "content",
+  schema: () =>
+    z.object({
+      title: z.string().max(65, {
+        message: "Title cannot be longer than 65 characters",
+      }),
+      description: z.string().max(165, {
+        message: "Description cannot be longer than 165 characters",
+      }),
+      pubDate: z.date(),
+      isDraft: z.boolean().optional(),
+    }),
+});
+
+export const collections = { posts };
--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -2,6 +2,9 @@ export const baseData = {
   title: "Cesar Poumian's Blog",
   description:
     "Welcome to my blog! I'm a software engineer and I write about programming, tech, and personal development.",
+  intro:
+    "Welcome to my blog! I'm a software engineer and I write about programming, tech, and personal development.",
+  greeting: "Hello, I'm Cesar",
 };
 
 export const socialData = [
